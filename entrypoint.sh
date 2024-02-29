55a2c2e245bdd86a36c4600aac2e36afab5eded7
Paul Bramhall <paulwamp@gmail.com>
2024-02-29 16:58:56 +0000

    Update Build Configuration
    Make sure all temporary build files are contained within a new `build-tmp` store which is ephemeral to the life cycle of the running Docker container used for building.
    
    Thie ensures we don't spam the `src` folder with compile-time artifacts and keeps the source tree clean.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,11 @@ set -e
 
 echo "Cleaning up old Build Files..."
 rm -rf ${APP_DIR}/build/*
-cd ${APP_DIR}/src
+echo "Creating Temporary Build Location..."
+mkdir -p ${APP_DIR}/build-tmp
+cd ${APP_DIR}/build-tmp
 echo "Cleaning old build/compile files (if any)..."
 rm -rf cmake_install.cmake CMakeCache.txt CMakeFiles elf2uf2 generated Makefile pico-sdk pioasm
-cmake .
+cmake ${APP_DIR}/src
 cmake --build .
 mv *.elf.map ../build/
